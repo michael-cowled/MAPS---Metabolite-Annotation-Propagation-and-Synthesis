ff29522fd5d48394d9d36cd161859ad4f9061992
Michael <74217435+michael-cowled@users.noreply.github.com>
2025-12-19 15:48:36 +1100

    Delete Scripts/#2__Comparing-Datasets directory

--- Scripts/#2__Comparing-Datasets/Comparing_Feature_Counts.R
+++
@@ -1,8 +0,0 @@
-dataset <- read.csv("Y:/MA_BPA_Microbiome/Dataset-Annotations/HGMD_0128.csv")
-
-usi_counts_dataset <- dataset %>%
-  group_by(confidence.level) %>%
-  summarize(total_usi = n_distinct(feature.usi), .groups = "drop")
-
-print(usi_counts_dataset)
-
